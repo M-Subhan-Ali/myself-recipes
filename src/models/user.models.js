78582c5eb69e707ef3a4341d4dfff2f80c25825d
M-Subhan-Ali <s8289552@gmail.com>
2025-03-25 23:45:41 +0500

    user model enhance with sparse attribute for getting either email or username

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,29 @@
+import mongoose from "mongoose";
 
+const user_Schema = mongoose.Schema({
+  username : {
+    type : String,
+    required : true,
+    trim : true,
+    sparse : true,
+  },
+  email:{
+    type : String,
+    required : true,
+    sparse : true,
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  saved_Recipes:{
+    type : mongoose.Schema.Types.ObjectID,
+    ref : "Word_Recipe"
+  }
+},{timeStamps : true})
+
+
+export const Work_User = mongoose.model("Work_User" , user_Schema);
 
 
 
